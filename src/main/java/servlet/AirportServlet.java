3447bb3915083219692541194eda6a125871c30c
jadilovic <adilovic79@yahoo.com>
2019-12-10 21:53:31 +0100

    added name of the airport servlet

--- src/main/java/servlet/AirportServlet.java
+++ src/main/java/servlet/AirportServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
-        name = "HomeServlet", 
+        name = "AirportServlet", 
         urlPatterns = {"/airport"}
     )
 public class AirportServlet extends HttpServlet {
